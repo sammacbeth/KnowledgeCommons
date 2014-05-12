6610a4c3087e758db65ad77ada6336faccda6426
Sam Macbeth <sm1106@imperial.ac.uk>
2014-05-12 16:51:42 +0100

    Institution profit calculation.

--- src/test/java/kc/TestRules.java
+++ src/test/java/kc/TestRules.java
@@ -4,8 +4,12 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
+import org.apache.commons.math.stat.descriptive.DescriptiveStatistics;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -15,8 +19,11 @@ import uk.ac.imperial.einst.EInstSession;
 import uk.ac.imperial.einst.StubActor;
 import uk.ac.imperial.einst.access.RoleOf;
 import uk.ac.imperial.einst.ipower.Obl;
+import uk.ac.imperial.einst.micropay.Account;
 import uk.ac.imperial.einst.micropay.Invoice;
 import uk.ac.imperial.einst.micropay.Transfer;
+import uk.ac.imperial.einst.resource.Pool;
+import uk.ac.imperial.einst.resource.facility.Facility;
 import uk.ac.imperial.presage2.core.environment.EnvironmentSharedStateAccess;
 
 public class TestRules {
@@ -109,5 +116,54 @@ public class TestRules {
 				.equalsIgnoreT(new Transfer(a1, i, SUB_FEE)));
 
 		assertEquals(0, inst.ipower.getObligations(a2).size());
+
+	}
+
+	@Test
+	public void testInstitutionProfit() {
+		final double BORROW_LIM = 10;
+		final double SUB_FEE = 1.0;
+
+		session.LOG_WM = false;
+		DataInstitution i = new DataInstitution("i1", BORROW_LIM);
+		i.subscriptionFees.put("test", SUB_FEE);
+		session.insert(i);
+		session.insert(i.getAccount());
+		session.insert(new Facility(i, Collections.<Pool> emptySet(), 5, 1, 0,
+				0));
+
+		Actor a1 = new StubActor("a1");
+		Account ac1 = new Account(a1, 100);
+		Actor a2 = new StubActor("a2");
+		Account ac2 = new Account(a2, 100);
+		session.insert(new RoleOf(a1, i, "test"));
+		session.insert(new RoleOf(a2, i, "test"));
+		session.insert(ac1);
+		session.insert(ac2);
+
+		session.incrementTime();
+		
+		DescriptiveStatistics paid = new DescriptiveStatistics(10);
+		paid.addValue(5 + 1);
+		DescriptiveStatistics received = new DescriptiveStatistics(10);
+		received.addValue(0);
+		Set<Obl> added = new HashSet<Obl>();
+		for (int j = 0; j < 8; j++) {
+			Set<Obl> obls = new HashSet<Obl>();
+			obls.addAll(inst.ipower.getObligations(a1));
+			obls.addAll(inst.ipower.getObligations(a2));
+			obls.removeAll(added);
+			for (Obl o : obls) {
+				session.insert(o.getAction());
+				added.add(o);
+			}
+			received.addValue(2);
+			paid.addValue(1);
+			session.incrementTime();
+			double balance = received.getSum() - paid.getSum();
+			assertEquals(balance, i.account.getBalance(), 0.00001);
+			assertEquals(balance / (1.0+j), i.profit, 0.00001);
+		}
+		assertTrue(true);
 	}
 }
